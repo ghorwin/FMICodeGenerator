94846cb6375da80171aaf3bef815b8d4f6f40556
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2019-03-06 05:57:06 -0400

    Added ui files for wizard pages.

---
+++ scripts/ui/readme.md
@@ -0,0 +1,8 @@
+# Qt-Ui files for the graphical user interface
+
+UI-files are converted to Python scripts:
+
+```bash
+> pyuic5 -x file.ui > -o file.py
+```
+
---
+++ scripts/ui/update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# -x     - generate test code to show and test the class
+# -i 0   - generate class using tabs as indentation
+
+pyuic5 -x -i 0 WizardPageBasicProperties.ui -o WizardPageBasicProperties.py
+pyuic5 -x -i 0 WizardPageVariables.ui -o WizardPageVariables.py
+

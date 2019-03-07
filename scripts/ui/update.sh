eca252d3f13e8cac08bdbe71fe92192337fc33de
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2019-03-07 15:43:23 -0400

    Added ui file for generation page.

--- scripts/ui/update.sh
+++ scripts/ui/update.sh
@@ -5,6 +5,7 @@
 
 pyuic5 -i 0 WizardPageBasicProperties.ui -o Ui_WizardPageBasicProperties.py
 pyuic5 -i 0 WizardPageVariables.ui -o Ui_WizardPageVariables.py
+pyuic5 -i 0 WizardPageGenerate.ui -o Ui_WizardPageGenerate.py
 
 
 # also transform the qrc file

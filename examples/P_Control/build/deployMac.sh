57261a6c338c216308fb2ea07cfd3ca28bbccade
Andreas Nicolai <andreas.nicolai@tu-dresden.de>
2021-02-03 15:17:10 +0100

    Added example deploy script for MacOS.

---
+++ examples/P_Control/build/deployMac.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# script is supposed to be executed in /build directory
+
+# remove target directory if it exists
+if [ -d P_Control ]; then
+  rm -rf P_Control 
+fi &&
+
+# remove target FMU if it exists
+if [ -f P_Control.fmu ]; then
+    rm P_Control.fmu 
+fi &&
+
+# create subdir and change into it
+mkdir -p P_Control &&
+cd P_Control &&
+
+# create binary dir for Linux
+mkdir -p binaries/darwin64 &&
+
+# copy shared library, we expect it to be already renamed correctly
+cp ../../bin/release/P_Control.dylib binaries/darwin64/P_Control.dylib &&
+cp ../../data/modelDescription.xml . &&
+
+# create zip archive
+zip -r ../P_Control.zip . | cat > /dev/null &&
+cd .. &&
+mv P_Control.zip P_Control.fmu &&
+echo "Created P_Control.fmu" &&
+
+# change working directory back to original dir
+cd -
+
